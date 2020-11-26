a525f75d2d6d0c050a976b2b3973de2cf520b8b5
estebandalelr <estebandalelr@gmail.com>
2020-11-26 17:02:41 -0600

    Improve placing of cards

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,5 @@
 const MovieCard = ({movie}) =>
-  <article className="shadow rounded">
+  <article className="shadow rounded flex flex-col items-center justify-center">
     <h3 className="text-2xl">
       {movie.original_title}
     </h3>
