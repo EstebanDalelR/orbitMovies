7e5bd710fd7bfc201a2a7112084c20d0be5d17ec
estebandalelr <estebandalelr@gmail.com>
2020-11-26 16:58:28 -0600

    Pull movies from server, show movie cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,25 @@
-const index = () => {
+import { InferGetServerSidePropsType } from 'next'
+
+const index = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <div>
-      hi
+      <div className="flex flex-wrap justify-around">
+      {data.results.map((movie, i) => <MovieCard movie={movie} key={i} />)}
+      </div>
     </div>
   )
 }
 export default index
+import { GetServerSideProps } from 'next'
+import MovieCard from '../components/MovieCard'
+type Data = { title: string }
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.TMDB_KEY}`)
+  const data: Data = await res.json()
+
+  return {
+    props: {
+      data,
+    },
+  }
+}
