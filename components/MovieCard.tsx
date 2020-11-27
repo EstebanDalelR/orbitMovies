b534b0bbeb706926ad55a2ec5b7e2238d3c19972
estebandalelr <estebandalelr@gmail.com>
2020-11-26 18:05:38 -0600

    Added img to card using string

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,8 @@
-const MovieCard = ({movie}) =>
-  <article className="shadow rounded flex flex-col items-center justify-center">
+import Image from "next/image"
+
+const MovieCard = ({ movie, imgUrl }) =>
+  <article className="shadow rounded flex flex-col items-center justify-center w-64 m-4 p-2">
+    <Image src={`${imgUrl}${movie.backdrop_path}`} width={250} height={130} />
     <h3 className="text-2xl">
       {movie.original_title}
     </h3>
