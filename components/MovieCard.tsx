0eaee5e44e229a8d8b54b8a8f1580fbc67b6989d
estebandalelr <estebandalelr@gmail.com>
2020-11-26 18:36:17 -0600

    Added individual movies pages

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image"
+import Link from "next/link"
 
 const MovieCard = ({ movie, imgUrl }) =>
-  <article className="shadow rounded flex flex-col items-center justify-center w-96 m-4 p-2 bg-white">
-    <Image src={`${imgUrl}${movie.backdrop_path}`} width={350} height={250} />
-    <h3 className="text-2xl">
-      {movie.original_title}
-    </h3>
-    <p>{movie.popularity}</p>
-  </article>
+  <Link href={`movies/${movie.id}`}>
+    <a>
+
+      <article className="shadow rounded flex flex-col items-center justify-center w-96 m-4 p-2 bg-white">
+        <Image src={`${imgUrl}${movie.backdrop_path}`} width={350} height={250} />
+        <h3 className="text-2xl">
+          {movie.original_title}
+        </h3>
+        <p className="text-xl">{movie.vote_average}/10</p>
+      </article>
+    </a>
+  </Link>
 export default MovieCard
---
+++ pages/movies/[id].tsx
@@ -0,0 +1,40 @@
+const Movie = ({ data }) => {
+  let { movie, config } = data
+  let imgUrl = `${config.images.base_url}${config.images.poster_sizes[5]}`
+  return (
+    <div>
+      <div className="flex flex-wrap">
+        <Image src={`${imgUrl}${movie.backdrop_path}`} width={350} height={250} />
+        <div className="mx-8 flex flex-col items-start justify-around">
+          <h1 className="text-4xl">{movie.original_title}</h1>
+          <h3>{movie.title}</h3>
+          <p>Original launch: {movie.release_date}</p>
+          <div className="w-full flex justify-evenly">
+            <p className="text-xl">{movie.vote_average}/10</p>
+            <p className="text-xl">{movie.vote_count} votes</p>
+          </div>
+        </div>
+      </div>
+      <div className="w-full my-4">
+        <p>{movie.overview}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Movie
+
+import { GetServerSideProps } from 'next'
+import Image from 'next/image'
+type Data = { movie, config }
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const movie = await fetch(`https://api.themoviedb.org/3/movie/${context.params.id}?api_key=${process.env.TMDB_KEY}`)
+  const config = await fetch(`https://api.themoviedb.org/3/configuration?api_key=${process.env.TMDB_KEY}`)
+  const data: Data = { movie: await movie.json(), config: await config.json() }
+
+  return {
+    props: {
+      data,
+    },
+  }
+}
