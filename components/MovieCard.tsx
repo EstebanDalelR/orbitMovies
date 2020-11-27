114ce62f65b2e328a9fe499ab5df8d5e62d18b0f
estebandalelr <estebandalelr@gmail.com>
2020-11-26 18:10:32 -0600

    Resize card, make them white

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image"
 
 const MovieCard = ({ movie, imgUrl }) =>
-  <article className="shadow rounded flex flex-col items-center justify-center w-64 m-4 p-2">
-    <Image src={`${imgUrl}${movie.backdrop_path}`} width={250} height={130} />
+  <article className="shadow rounded flex flex-col items-center justify-center w-96 m-4 p-2 bg-white">
+    <Image src={`${imgUrl}${movie.backdrop_path}`} width={350} height={250} />
     <h3 className="text-2xl">
       {movie.original_title}
     </h3>
