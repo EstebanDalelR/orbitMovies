f17129c20410b9629857ac6d18d26670ad66aca8
estebandalelr <estebandalelr@gmail.com>
2020-12-17 13:47:51 -0600

    Use Houm color

--- pages/movies/[id].tsx
+++ pages/movies/[id].tsx
@@ -6,7 +6,7 @@ const Movie = ({ data }) => {
       <div className="flex flex-wrap">
         <Image src={`${imgUrl}${movie.backdrop_path}`} width={350} height={250} />
         <div className="mx-8 flex flex-col items-start justify-around">
-          <h1 className="text-4xl">{movie.original_title}</h1>
+          <h1 className="text-4xl text-houm">{movie.original_title}</h1>
           <h3>{movie.title}</h3>
           <p>Original launch: {movie.release_date}</p>
           <div className="w-full flex justify-evenly">
