d61adc847012a65b544f86a507914cd507f565bc
estebandalelr <estebandalelr@gmail.com>
2020-12-17 13:46:16 -0600

    Use Houm color

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -5,9 +5,9 @@ const MovieCard = ({ movie, imgUrl }) =>
   <Link href={`movies/${movie.id}`}>
     <a>
 
-      <article className="shadow rounded flex flex-col items-center justify-center w-96 m-4 p-2 bg-white">
+      <article className="shadow rounded flex flex-col items-center justify-center w-96 m-4 p-2 bg-white ">
         <Image src={`${imgUrl}${movie.backdrop_path}`} width={350} height={250} />
-        <h3 className="text-2xl">
+        <h3 className="text-2xl text-houm">
           {movie.original_title}
         </h3>
         <p className="text-xl">{movie.vote_average}/10</p>
